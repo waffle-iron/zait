e7399a1ce3d83e9183dc30956761fcba299266a1
ket4yii <ket4yiit@gmail.com>
2016-07-22 21:37:08 +0300

    Gulpfile cleanup. Closed #49

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,57 @@
-'use strict';
-
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const mocha = require('gulp-mocha');
 const eslint = require('eslint/lib/cli');
 const jscpd = require('gulp-jscpd');
 const jsdoc = require('gulp-jsdoc3');
-const shell = require('gulp-shell');
 
 const spawn = require('child_process').spawn;
 
-//config
+// config
 const paths = {
   from: 'src/**/*.js',
   to: 'lib/'
 };
 
-gulp.task('lint', function (pipeline) {
+gulp.task('lint', pipeline => {
   const exitCode = eslint.execute('--ext .js src');
   pipeline(exitCode);
 });
 
-gulp.task('jscpd', function () {
-  return gulp.src(paths.from, { base: '.' })
-    .pipe(jscpd());
-});
+gulp.task('jscpd', () => gulp
+  .src(paths.from, { base: '.' })
+  .pipe(jscpd())
+);
 
-gulp.task('jsdoc', function () {
+gulp.task('jsdoc', () => {
   const jsDocConf = require('./jsdocConf.json');
 
   return gulp.src(paths.from, { base: '.', read: false })
-    .pipe(jsdoc());
+    .pipe(jsdoc(jsDocConf));
 });
 
-gulp.task('test', function (pipeline) {
+gulp.task('test', pipeline => {
   const isparta = spawn('./test/.isparta', {
     stdio: 'inherit'
   });
 
-  isparta.on('close', function (code) {
+  isparta.on('close', code => {
     pipeline(code);
   });
 });
 
-gulp.task('prepare-casper-test', function () {
-  return gulp.src('./test/casperjs/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./build/test'));
-});
+gulp.task('js', () => gulp
+  .src(paths.from)
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(gulp.dest(paths.to))
+);
 
-gulp.task('casper-test', ['prepare-casper-test'], function () {
-  return gulp.src('./build/test/*.js', { read: false })
-    .pipe(shell(['./node_modules/mocha-casperjs/bin/mocha-casperjs ./build/test/*.js', 'rm -rf ./build/test']))
-});
-
-gulp.task('js', function () {
-  return gulp.src(paths.from)
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest(paths.to));
-});
-
-gulp.task('build', ['test', 'lint', 'js', 'jsdoc']);
+gulp.task('build', ['test', 'lint', 'js']);
 
 gulp.task('default', ['js']);
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch(paths.from, ['js']);
 });
 
